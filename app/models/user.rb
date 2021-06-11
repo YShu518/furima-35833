d21593dc032b0c19501579706e51d07980a5b30c
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-11 18:35:24 +0900

    第一修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/,
+                           message: 'is invalid. Input full-width katakana characters' } do
+      validates :first_name_katakana
+      validates :last_name_katakana
+    end
+    
+  end
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
                                  message: 'is invalid. Include both letters and numbers' }
-  validates :birthday, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
-  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-                                                            message: 'is invalid. Input full-width katakana characters' }
-  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-                                                           message: 'is invalid. Input full-width katakana characters' }
 end
