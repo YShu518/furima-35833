c70d2eee9d195fb7bc7ce79c26b41f009156c893
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-15 14:26:51 +0900

    テーブルオプション設定

--- db/migrate/20210614035422_create_items.rb
+++ db/migrate/20210614035422_create_items.rb
@@ -2,15 +2,15 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.string :name
-      t.text :description
-      t.integer :category_id
-      t.integer :used_id
-      t.integer :price
-      t.integer :area_id
-      t.integer :shopping_cost_id
-      t.integer :takes_day_id
-      t.references :user, foreign_key: true
+      t.string :name, null: false
+      t.text :description, null:false
+      t.integer :category_id, null:false
+      t.integer :used_id, null:false
+      t.integer :price, null:false
+      t.integer :area_id, null:false
+      t.integer :shopping_cost_id, null:false
+      t.integer :takes_day_id, null:false
+      t.references :user, null:false, foreign_key: true
       t.timestamps
     end
   end
