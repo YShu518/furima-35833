05e925ea6116bfec4a3894f8864a211229ff8300
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-15 15:18:22 +0900

    category作成、Gem導入
    categoryのActiveHash作成
    mini_magick, image_processingの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
     validates :used_id
     validates :price
     validates :area_id
@@ -10,5 +9,10 @@ class Item < ApplicationRecord
     validates :takes_day_id
   end
 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
 end
