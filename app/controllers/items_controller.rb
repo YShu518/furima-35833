d74fba7aed8a98f13b7638ed601e3c57ea1a17cf
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-15 17:04:15 +0900

    各ActiveHashの形成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
     
   end
 
   def new
-    @item = Item.new
+    @item = Item.new.include(:user)
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :used_id, :shopping_cost_id, :area_id, :takes_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,18 +2,19 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :used_id
     validates :price
-    validates :area_id
-    validates :shopping_cost_id
-    validates :takes_day_id
   end
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
+    validates :area_id
+    validates :shopping_cost_id
+    validates :takes_day_id
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :used
+  belongs_to :shopping_cost
   belongs_to :user
   has_one_attached :image
 end
