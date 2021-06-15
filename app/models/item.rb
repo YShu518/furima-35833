0820bd6cb27a8f868cf57a74b31457a6f6a18427
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-15 14:34:19 +0900

    ヴァリテーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
-  
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :used_id
+    validates :price
+    validates :area_id
+    validates :shopping_cost_id
+    validates :takes_day_id
+  end
+
+  belongs_to :user
 end
