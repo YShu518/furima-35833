7b87d50a86d3b32c2da2a24a7c0046c971038fe2
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-24 12:46:35 +0900

    細かい修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :area_id, numericality: { other_than: 1 }
     validates :city
     validates :address_line1
@@ -15,6 +15,7 @@ class OrderAddress
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id, token: token)
-    Address.create(postal_code: postal_code, area_id: area_id, city: city, address_line1: address_line1, address_line2: address_line2, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, address_line1: address_line1,
+                   address_line2: address_line2, phone_number: phone_number, order_id: order.id)
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe OrderAddress, type: :model do
   end
 
   describe '購入情報登録' do
-    context '登録できる場合'do
+    context '登録できる場合' do
       it '全ての情報が存在すれば登録できる' do
         expect(@order_address).to be_valid
       end
@@ -19,7 +19,7 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address).to be_valid
       end
 
-      it 'phone_numberが10桁でも登録できる' do 
+      it 'phone_numberが10桁でも登録できる' do
         @order_address.phone_number = '0123456789'
         expect(@order_address).to be_valid
       end
@@ -41,19 +41,19 @@ RSpec.describe OrderAddress, type: :model do
       it 'postal_cordが半角数字のみの場合' do
         @order_address.postal_code = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
 
       it 'postal_cordが半角数字とハイフン以外を含む場合' do
         @order_address.postal_code = 'a12-3456'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
 
       it 'postal_cordが全角数字では登録できない' do
-        @order_address.postal_code ='１２３-４５６７'
+        @order_address.postal_code = '１２３-４５６７'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
 
       it 'area_idが空だと登録できない' do
@@ -95,31 +95,31 @@ RSpec.describe OrderAddress, type: :model do
       it 'phone_numberが9桁以下の場合' do
         @order_address.phone_number = '012345678'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberが12桁以上の場合' do
         @order_address.phone_number = '012345678901'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberが0以外から始まる場合' do
         @order_address.phone_number = '1234567890'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberが全角数字の場合' do
         @order_address.phone_number = '０１２３４５６７８９０'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'phone_numberが半角数字以外を含む場合' do
         @order_address.phone_number = '012-345-6789'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
 
       it 'user_idが存在しない場合' do
