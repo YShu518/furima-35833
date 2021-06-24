60767af85c196b1dceffae71cb00dea886a022b1
YShu518 <84174337+YShu518@users.noreply.github.com>
2021-06-24 16:25:22 +0900

    コードレビュー修正２

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -3,10 +3,6 @@ FactoryBot.define do
     transient do
       address { Gimei.address }
     end
-    # association :user
-    # association :item
-    # user_id { user.id }
-    # item_id { item.id }
     token { 'tok_abcdefghijk00000000000000000' }
     postal_code { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4) }
     area_id { Faker::Number.between(from: 2, to: 48) }
